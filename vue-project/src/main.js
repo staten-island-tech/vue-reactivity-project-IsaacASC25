ea73d432351ec7c0ef7693b049a481eee7757e33
Lunacywonders <Isaacn9394@gmail.com>
2026-02-11 12:10:14 -0500

    asd

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
+import CookieClicker from '@/views/CookieClicker.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path:'/',
+      name:'home',
+      component: CookieClicker,
+    },
+  ],
+})
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+export default router
